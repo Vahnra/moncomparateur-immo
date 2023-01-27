cfcff741a5f80f0567ea6b9135f92fdbd804ebb6
Vahnra <102360714+Vahnra@users.noreply.github.com>
2023-01-27 18:08:01 +0100

    v0.1

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -5,6 +5,7 @@ import { GoogleMapsModule } from '@angular/google-maps';
 import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
 import { DvfMapsComponent } from './dvf-maps/dvf-maps.component';
 import { SitadelMapsComponent } from './sitadel-maps/sitadel-maps.component';
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 
 
 
@@ -19,6 +20,11 @@ import { SitadelMapsComponent } from './sitadel-maps/sitadel-maps.component';
     GoogleMapsModule,
     HttpClientModule,
     HttpClientJsonpModule,
+    NgxMapboxGLModule.withConfig({
+      accessToken: 'pk.eyJ1IjoidmFobnJhIiwiYSI6ImNsZGVsczc0MDBlcmIzbm82ODUwbG9yeXYifQ.TgTZQqBR2BhVt8YEA6NPpw', 
+    }),
   ]
 })
-export class MapsModule { }
+export class MapsModule { 
+  
+}
