354f9a9f611ea6168b4d2d85fe74086bec8fa31b
Vahnra <102360714+Vahnra@users.noreply.github.com>
2023-01-28 16:22:54 +0100

    v0.5 leaflet

--- src/app/maps/maps-view/maps-view.component.ts
+++ src/app/maps/maps-view/maps-view.component.ts
@@ -3,8 +3,10 @@ import { HttpClient } from '@angular/common/http';
 
 import { MapsService } from '../maps.service';
 import { circle, latLng, polygon, tileLayer, Map, marker, icon, Icon, LayerGroup } from 'leaflet';
+import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
 import { Dpe } from 'src/app/models/dpe';
 import * as Leaflet from 'leaflet';
+
 
 
 @Component({
@@ -17,8 +19,15 @@ export class MapsViewComponent implements OnInit {
   enableCall: boolean = true;
 
   pbe: LayerGroup = new LayerGroup();
+  provider = new OpenStreetMapProvider();
 
   lookup: any = [];
+  layerMainGroup: LayerGroup[] = [];
+  center: any;
+
+  customOptions = {
+    'minWidth': 300, 
+   }
   
   options = {
     layers: [
@@ -30,17 +39,62 @@ export class MapsViewComponent implements OnInit {
 
   layersControl = {
     baseLayers: {
-      'Open Street Map': tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }),
-      'Open Cycle Map': tileLayer('https://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+      'Plan': tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }),
+      'Vue satellite': tileLayer('https://wxs.ign.fr/{ignApiKey}/geoportail/wmts?'+
+      '&REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&TILEMATRIXSET=PM'+
+      '&LAYER={ignLayer}&STYLE={style}&FORMAT={format}'+
+      '&TILECOL={x}&TILEROW={y}&TILEMATRIX={z}', {
+        ignApiKey: 'decouverte',
+        ignLayer: 'ORTHOIMAGERY.ORTHOPHOTOS',
+        style: 'normal',
+        format: 'image/jpeg',
+        service: 'WMTS'
+      }),
+      'Plan IGN': tileLayer('https://wxs.ign.fr/{ignApiKey}/geoportail/wmts?'+
+            '&REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&TILEMATRIXSET=PM'+
+            '&LAYER={ignLayer}&STYLE={style}&FORMAT={format}'+
+            '&TILECOL={x}&TILEROW={y}&TILEMATRIX={z}',
+            {
+	            ignApiKey: 'decouverte',
+	            ignLayer: 'GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2',
+	            style: 'normal',
+	            format: 'image/png',
+	            service: 'WMTS',         
+      }),
+      'Plan cadastre': tileLayer('https://wxs.ign.fr/{apikey}/geoportail/wmts?REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&STYLE={style}&TILEMATRIXSET=PM&FORMAT={format}&LAYER=CADASTRALPARCELS.PARCELLAIRE_EXPRESS&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}', {
+        attribution: '<a target="_blank" href="https://www.geoportail.gouv.fr/">Geoportail France</a>',
+        bounds: [[-75, -180], [81, 180]],
+        minZoom: 2,
+        maxZoom: 19,
+        apikey: 'choisirgeoportail',
+        format: 'image/png',
+        style: 'PCI vecteur'
+      })
     },
     overlays: {
-      'test': this.pbe
+      'PBE': this.pbe
     }
   }
-  
-  layerMainGroup: LayerGroup[] = [];
 
-  center: any;
+  customPopup = `<div class="row" style="margin-bottom: 0;>
+    <div class="col s12" style="margin-bottom: 0;">
+      <div class="card z-depth-0" style="margin-bottom: 0;">
+    
+          <span class="col s6 right-align"><strong>N°DPE</strong></span><span class="col s6">{{ dpeNumber }}</span>
+          <span class="col s6 right-align"><strong>Date DPE</strong></span><span class="col s6">{{ dpeDate }}</span>
+          <span class="col s6 right-align"><strong>Etiquette DPE</strong></span><span class="col s6">{{ consommation }}</span>
+          <span class="col s6 right-align"><strong>Adresse</strong></span><span class="col s6">{{ adress }}</span>
+          <!-- <span class="col s6 right-align">Complément d'adresse</span><span class="col s6">{{ moreAdress }}</span> -->
+          <span class="col s6 right-align"><strong>Type</strong></span><span class="col s6">{{ typeOfLodging }}</span>
+          <!-- <span class="col s6 right-align">Année de construction</span><span class="col s6">{{ buildingDate }}</span> -->
+          <span class="col s6 right-align"><strong>Surface</strong></span><span class="col s6">{{ areaSize }}</span> 
+          <a class="center-align col s12" href="">Ajouter aux favoris</a>
+  
+      </div>
+    </div>
+  </div>`
+  
+
 
   constructor(public httpClient: HttpClient, private mapsService: MapsService) {
  
@@ -54,8 +108,18 @@ export class MapsViewComponent implements OnInit {
   
 
   test: Dpe;
+  
 
   onMapReady(map: Map) {
+    // add geosearch
+    
+    const searchControl = GeoSearchControl({
+      provider: this.provider,
+      style:"bar"
+    });
+    map.addControl(searchControl);
+    
+    map.addLayer(this.pbe)
     // console.log(map.getCenter());
     navigator.geolocation.getCurrentPosition((position) => {
       // console.log(position);
@@ -72,26 +136,44 @@ export class MapsViewComponent implements OnInit {
 
           if (this.isLocationFree(nameArr) == true) {
 
-            this.lookup.push([nameArr[0], nameArr[1]]);
+            if (item['Date_établissement_DPE'] > '2022-09-01') {
 
-            marker([ +nameArr[0], +nameArr[1] ], {
-              icon: icon({
-                ...Icon.Default.prototype.options,
-                iconUrl: 'assets/marker-icon.png',
-                iconRetinaUrl: 'assets/marker-icon-2x.png',
-                shadowUrl: 'assets/marker-shadow.png'
-              })
-            }).bindPopup('test').openPopup().addTo(this.pbe);
-          }  
+              this.lookup.push([nameArr[0], nameArr[1]]);
+              let customPopup = `
+                <div class="row" style="margin-bottom: 0;>
+                  <div class="col s12" style="margin-bottom: 0;">
+                    <div class="card z-depth-0" style="margin-bottom: 0;">
+                  
+                        <span class="col s6 right-align"><strong>N°DPE</strong></span><span class="col s6">${item['N°DPE']}</span>
+                        <span class="col s6 right-align"><strong>Date DPE</strong></span><span class="col s6">${item['Date_établissement_DPE']}</span>
+                        <span class="col s6 right-align"><strong>Etiquette DPE</strong></span><span class="col s6">${item['Etiquette_DPE']}</span>
+                        <span class="col s6 right-align"><strong>Adresse</strong></span><span class="col s6">${item['Adresse_(BAN)']}</span>
+                        <!-- <span class="col s6 right-align">Complément d'adresse</span><span class="col s6">${item['Complément_d\'adresse_logement']}</span> -->
+                        <span class="col s6 right-align"><strong>Type de logement</strong></span><span class="col s6">${item['Type_bâtiment']}</span>
+                        <!-- <span class="col s6 right-align">Année de construction</span><span class="col s6">${item['Année_construction']}</span> -->
+                        <span class="col s6 right-align"><strong>Surface habitable</strong></span><span class="col s6">${item['Surface_habitable_logement']}</span> 
+                        <a class="center-align col s12" href="">Ajouter aux favoris</a>
+                
+                    </div>
+                  </div>
+                </div>`
+
+              marker([ +nameArr[0], +nameArr[1] ], {
+                icon: icon({
+                  ...Icon.Default.prototype.options,
+                  iconUrl: 'assets/marker-icon.png',
+                  iconRetinaUrl: 'assets/marker-icon-2x.png',
+                  shadowUrl: 'assets/marker-shadow.png'
+                })
+              }).bindPopup(customPopup, this.customOptions).openPopup().addTo(this.pbe);
+            } // End check date
+          } //End if not already in marker
           
         }) //End of foreach
 
       }) // End of map service call
 
     })// End of get current position
-
-    
-    
 
   }
 
@@ -100,8 +182,6 @@ export class MapsViewComponent implements OnInit {
 
     if (!this.enableCall) return;
     this.enableCall = false;
-
-    console.log($event.target.getCenter());
 
     this.mapsService.refreshDpe($event.target.getCenter().lat, $event.target.getCenter().lng).subscribe(data => {
 
@@ -114,20 +194,39 @@ export class MapsViewComponent implements OnInit {
 
         if (this.isLocationFree(nameArr) == true) {
 
-          this.lookup.push([nameArr[0], nameArr[1]]);
+            if (item['Date_établissement_DPE'] > '2022-09-01') {
 
-          marker([ +nameArr[0], +nameArr[1] ], {
-            icon: icon({
-              ...Icon.Default.prototype.options,
-              iconUrl: 'assets/marker-icon.png',
-              iconRetinaUrl: 'assets/marker-icon-2x.png',
-              shadowUrl: 'assets/marker-shadow.png'
-            })
-          }).bindPopup('test').openPopup().addTo(this.pbe);
-        }  
-        
+            this.lookup.push([nameArr[0], nameArr[1]]);
+            let customPopup = `
+              <div class="row" style="margin-bottom: 0;>
+                <div class="col s12" style="margin-bottom: 0;">
+                  <div class="card z-depth-0" style="margin-bottom: 0;">
+                
+                      <span class="col s6 right-align"><strong>N°DPE</strong></span><span class="col s6">${item['N°DPE']}</span>
+                      <span class="col s6 right-align"><strong>Date DPE</strong></span><span class="col s6">${item['Date_établissement_DPE']}</span>
+                      <span class="col s6 right-align"><strong>Etiquette DPE</strong></span><span class="col s6">${item['Etiquette_DPE']}</span>
+                      <span class="col s6 right-align"><strong>Adresse</strong></span><span class="col s6">${item['Adresse_(BAN)']}</span>
+                      <!-- <span class="col s6 right-align">Complément d'adresse</span><span class="col s6">${item['Complément_d\'adresse_logement']}</span> -->
+                      <span class="col s6 right-align"><strong>Type de logement</strong></span><span class="col s6">${item['Type_bâtiment']}</span>
+                      <!-- <span class="col s6 right-align">Année de construction</span><span class="col s6">${item['Année_construction']}</span> -->
+                      <span class="col s6 right-align"><strong>Surface habitable</strong></span><span class="col s6">${item['Surface_habitable_logement']}</span> 
+                      <a class="center-align col s12" href="">Ajouter aux favoris</a>
+              
+                  </div>
+                </div>
+              </div>`
+
+            marker([ +nameArr[0], +nameArr[1] ], {
+              icon: icon({
+                ...Icon.Default.prototype.options,
+                iconUrl: 'assets/marker-icon.png',
+                iconRetinaUrl: 'assets/marker-icon-2x.png',
+                shadowUrl: 'assets/marker-shadow.png'
+              })
+            }).bindPopup(customPopup, this.customOptions).openPopup().addTo(this.pbe);
+          }  
+        }      
       }) //End of foreach
-
     }) // End of map service call
     
     setTimeout(() => this.enableCall = true, 1000);
@@ -135,13 +234,12 @@ export class MapsViewComponent implements OnInit {
  
   isLocationFree(search: any) {
     for (var i = 0, l = this.lookup.length; i < l; i++) {
-      if (this.lookup[i][0] === search[0] && this.lookup[i][1] === search[1]) {
-        console.log(false);
-        
+      if (this.lookup[i][0] === search[0] && this.lookup[i][1] === search[1]) {    
         return false
       }
-    }
-    
+    }   
     return true
   }
+
+  
 }
