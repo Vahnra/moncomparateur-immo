23d82115f35e33b1d8667bee5bf417991ae6d6b9
Vahnra <102360714+Vahnra@users.noreply.github.com>
2023-02-19 02:33:56 +0100

    Update user-calendar.component.ts

--- src/app/user/user-calendar/user-calendar.component.ts
+++ src/app/user/user-calendar/user-calendar.component.ts
@@ -116,7 +116,7 @@ export class UserCalendarComponent implements OnInit {
                 let event: any = {};
                 event['title'] = element.title;
                 event['start'] = new Date(element.start);
-                event["allDay"] = element.allDay;
+                event["allDay"] = true;
                 this.events.push(event);
               });  
             }, error: err => {
